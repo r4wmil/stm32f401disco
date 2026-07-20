4cadd4fb176e570f3f9fc83f170c8b40bd6ee7ad
Ramil Khasanshin <ramil345655@gmail.com>
2026-07-21 01:40:39 +0300

    refactor(blink_1): useful info

--- 1_blink/main.c
+++ 1_blink/main.c
@@ -5,13 +5,21 @@
 #define LED3 14
 #define LED4 15
 
-void init_pin(unsigned int PIN) {
-	GPIOD->MODER &= ~(3U << (PIN * 2));
-	GPIOD->MODER |=  (1U << (PIN * 2));
+void init_pin(uint32_t PIN) {
+	// MODER - MODE Register
+	//  00 - input
+	// >01 - general-purpose output
+	//  10 - alternate function (UART, SPI, I2C, TIM, etc.)
+	//  11 - analog (ADC/DAC or low-power)
+	GPIOD->MODER &= ~(3U << (PIN * 2)); // Clear
+	GPIOD->MODER |=  (1U << (PIN * 2)); // Set 01
 }
 
 int main(void)
 {
+	// GPIOD - General Purpose I/O "D"
+	// RCC - Reset & Clock Controller
+	// AHB1ENR - Advanced High-performance Bus 1 ENable Register
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 
 	init_pin(LED1);
@@ -21,6 +29,7 @@ int main(void)
 
 	while (1)
 	{
+		// ODR - Output Data Register
 		GPIOD->ODR ^= (1U << LED1);
 		for (volatile uint32_t i = 0; i < 500000; i++);
 		GPIOD->ODR ^= (1U << LED2);
